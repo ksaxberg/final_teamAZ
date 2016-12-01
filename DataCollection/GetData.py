4f4de21c8b1e74263ccab1005db75b5438274536
Adele Kapellusch <akoutia@email.arizona.edu>
2016-12-01 15:36:17 -0700

    Changed method for data collection
    Changed the method to use openDap and processing is done in a separate
    program

--- DataCollection/GetData.py
+++ DataCollection/GetData.py
@@ -1,105 +1,89 @@
-import pyGDP
+from netCDF4 import Dataset
+import numpy as np
+import pandas as pd
 import sys
-import os
-import datetime
 
-#[0] = state [1] = historical, future rcp45, or future rcp85 [2] = zone [3]= # of years (default all) [4] = model (default GFDL-ESM2M)
-commandlineArgs = sys.argv[1:]
+#ARGUMENTS: data type, lat, lon
+arguments = sys.argv[1:]
+dataType = arguments[0] #historical, rcp45, rcp85
+latitude = float(arguments[1])
+longitude = float(arguments[2])
 
-if len(commandlineArgs) < 3 or len(commandlineArgs) > 5:
-    sys.exit("ERROR: Incorrect number of arguments! (state, data type, zone, number of years(optional), and model (optional)")
 
-state = commandlineArgs[0]
-model = commandlineArgs[1]
-climateModel = 'GFDL-ESM2M'
-if len(commandlineArgs) == 4 or len(commandlineArgs) == 5:
-    yrs = commandlineArgs[3]
-    if len(commandlineArgs) == 5:
-        climateModel = commandlineArgs[4]
+outputFile = dataType + '_' + str(latitude) + '_' + str(longitude) + '.csv'
+day = 1  #start day
 
-pyGDP = pyGDP.pyGDPwebProcessing()  # initialize a pyGDP wps object
-
-filePath = '/final_teamAZ/DataCollection/StateGrids/state_' + state + 'Grid.zip'  #which shapefile to use
-
-# upload the file to geoserver
-try:
-    shpfile = pyGDP.uploadShapeFile(filePath)
-
-except Exception:
-    shpfile = 'upload:state_' + state + 'Grid'
+#Set lat/lon pairs according to north and east reference frame
+if latitude < 0:
+    lat_target = 360+latitude
 else:
-    print 'Shapefile Uploaded'
+    lat_target = latitude
 
-# Grab the file and set its attributes:
-Stateshapefile = shpfile
+if longitude < 0:
+    lon_target = 360 + longitude
+else:
+    lon_target = longitude
 
-usr_attribute = 'id'
-usr_value = commandlineArgs[2]
-
-if model == 'historical':
-    datasetURI = 'http://cida.usgs.gov/thredds/dodsC/macav2metdata_daily_historical'
-    dataType = ['tasmax_' + climateModel+ '_r1i1p1_historical', 'tasmin_' + climateModel + '_r1i1p1_historical',
-                'pr_' + climateModel +'_r1i1p1_historical','rhsmax_' + climateModel+ '_r1i1p1_historical',
-                'rhsmin_' + climateModel + '_r1i1p1_historical']
-    timeStart = '1950-01-01T00:00:00.000Z'
-    if len(commandlineArgs) == 3:
-        timeEnd = '2005-12-31T00:00:00.000Z'
-    if len(commandlineArgs) == 4:
-        finalYear = 1949 + int(yrs)
-        timeEnd = str(finalYear) + '-12-31T00:00:00.000Z'
-    time = 'hist'
-if model == 'future45':
-    datasetURI = 'http://cida.usgs.gov/thredds/dodsC/macav2metdata_daily_future'
-    dataType = ['tasmax_' + climateModel + '_r1i1p1_rcp45', 'tasmin_' + climateModel +'_r1i1p1_rcp45',
-                'pr_' + climateModel +'_r1i1p1_rcp45','rhsmax_' + climateModel + '_r1i1p1_rcp45',
-                'rhsmin_' + climateModel + '_r1i1p1_rcp45']
-    timeStart = '2006-01-01T00:00:00.000Z'
-    if len(commandlineArgs) == 3:
-        timeEnd = '2099-12-31T00:00:00.000Z'
-    if len(commandlineArgs) == 4:
-        finalYear = 2005 + int(yrs)
-        timeEnd = str(finalYear) + '-12-31T00:00:00.000Z'
-    time = 'fut45'
-if model == 'future85':
-    datasetURI = 'http://cida.usgs.gov/thredds/dodsC/macav2metdata_daily_future'
-    dataType = ['tasmax_' + climateModel + '_r1i1p1_rcp85', 'tasmin_' + climateModel +'_r1i1p1_rcp85',
-                'pr_' + climateModel +'_r1i1p1_rcp85','rhsmax_' + climateModel + '_r1i1p1_rcp85',
-                'rhsmin_' + climateModel + '_r1i1p1_rcp85']
-    timeStart = '2006-01-01T00:00:00.000Z'
-    if len(commandlineArgs) == 3:
-        timeEnd = '2099-12-31T00:00:00.000Z'
-    if len(commandlineArgs) == 4:
-        finalYear = 2005 + int(yrs)
-        timeEnd = str(finalYear) + '-12-31T00:00:00.000Z'
-    time = 'fut85'
-
-print "Gathering Data"
-File_handle = pyGDP.submitFeatureWeightedGridStatistics(Stateshapefile, datasetURI, dataType, timeStart, timeEnd,
-                                                             usr_attribute, usr_value)
-
-input = File_handle
-output = climateModel + '_' + state + '_'+ time + '_' + timeStart[0:4] + '_' + timeEnd[0:4] + '_' + usr_value + '.csv'
-numberOfVariables = 5
-
-with open(input, 'r') as reader:
-    data = [x.replace("\n", ",").split(",") for x in reader.readlines()]
-factorToSkipBy = (len(data) / numberOfVariables)
-
-with open(output, 'w') as csv:
-    csv.write("Year,Month,Day,T_max,T_min,Precip(mm),T_ave,Precip(cm),rh_ave\n")
-    for i in range(3, factorToSkipBy - 1):
-        year, month, day = data[i][0].split("-")
-        csv.write(", ".join((year, month, day[:2], str(float(data[i][1]) -273.15), str(float(data[i + factorToSkipBy][1])-273.15),
-                         data[i + factorToSkipBy * 2][1],
-                         str(((float(data[i][1]) + float(data[i + factorToSkipBy][1])) / 2)-273.15),
-                         str(float(data[i + factorToSkipBy * 2][1]) / 10),
-                         str((float(data[i + factorToSkipBy * 3][1]) + float(
-                             data[i + factorToSkipBy * 4][1]) / 2))))+'\n')
-os.remove(File_handle)
-os.remove('owslib.log')
+#Based on data type, give the OPeNAP Dataset url
+if dataType == 'historical':
+    path = "http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_{}_GFDL-ESM2M_r1i1p1_historical_1950_2005_WUSA.nc"
+if dataType == 'rcp45':
+    path = "http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_{}_GFDL-ESM2M_r1i1p1_rcp45_2006_2100_WUSA.nc"
+if dataType == 'rcp85':
+    path = "http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav1metdata_{}_GFDL-ESM2M_r1i1p1_rcp85_2006_2100_WUSA.nc"
 
 
+VAR_PATHS = {
+    'tasmax': "air_temperature",
+    'tasmin': "air_temperature",
+    'pr': "precipitation_flux",
+    'rhsmax': "relative_humidity",
+    'rhsmin': "relative_humidity",
+}
+VAR_HEADER_MAP = {
+    'tasmax': "tmax",
+    'tasmin': "tmin",
+    'pr': "precip(mm)",
+    'rhsmax': "rhmax",
+    'rhsmin': "rhmin",
+}
 
+df = pd.DataFrame()
 
+# CREATE data handles
+for var, idx in VAR_PATHS.items():
+    fh = Dataset(path.format(var), 'r', format="NETCDF4")  # filehandle
+    lath = fh.variables['lat']  # latitude handle
+    lonh = fh.variables['lon']  # longitude handle
+    timeh = fh.variables['time']  # time handle
+    datah = fh.variables[idx]  # handle on the
+
+    # extract data
+    time_length = len(timeh)  # get the number of records
+    time_index = range(day - 1, time_length, 2000)  # step through the values one day at a time
+    time = timeh[time_index]
+    lat = lath[:]
+    lon = lonh[:]
+
+    # find the indices for the targer lat/lon
+    lat_index = np.searchsorted(lat, lat_target, side='left')
+    lon_index = np.searchsorted(lon, lon_target, side='left')
+
+    # check bounds of lat/lon (round up/down for the coordinate in the list)
+    if (lat[lat_index] > lat_target):
+        if (lat_index != 0):
+            lat_index = lat_index - 1
+    if (lat[lat_index] < lat_target):
+        if (lat_index != len(lat)):
+            lat_index = lat_index + 1
+    if (lon[lon_index] > lon_target):
+        if (lon_index != 0):
+            lon_index = lon_index - 1
+    if (lon[lon_index] < lon_target):
+        if (lon_index != len(lon)):
+            lon_index = lon_index + 1
+
+    df[VAR_HEADER_MAP[var]] = datah[time_index, lat_index, lon_index]
+df.to_csv(outputFile)
 
 
