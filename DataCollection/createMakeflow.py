56ed1ec0d5c70445a2e67398337c6830e314eba2
Adele Kapellusch <akoutia@email.arizona.edu>
2016-12-04 14:37:26 -0700

    Makeflow Ready to Go

---
+++ DataCollection/createMakeflow.py
@@ -0,0 +1,17 @@
+coorFile = "filteredArizonaCoords.csv"
+mfFile = 'Arizona.makeflow'
+
+with open(coorFile,'r') as input:
+    lines = input.readlines()
+
+points = []
+for line in lines:
+    points.append(line.split(','))
+
+with open(mfFile,'w') as output:
+    for point in points:
+        if point != points[0]:
+            lat = point [0]
+            lon = str(float(point[1]))
+            output.write(": MakeRequests.py DataProcessing.py\n")
+            output.write("\tpython MakeRequests.py " + lat + "," + lon +"\n")
