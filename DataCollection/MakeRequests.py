5dae43e7bb1b6a4aff652ee87d024fb1499752d2
Adele Kapellusch <akoutia@email.arizona.edu>
2016-12-04 17:17:57 -0700

    Updated Requests

--- DataCollection/MakeRequests.py
+++ DataCollection/MakeRequests.py
@@ -51,5 +51,5 @@ os.system(Request)
 os.system("python DataProcessing.py RAW_historical_" + lat +"_" + lon +".csv historical_" + lat +"_" + lon +".csv")
 os.system("python DataProcessing.py RAW_rcp45_" + lat +"_" + lon +".csv rcp45_" + lat +"_" + lon +".csv")
 os.system("python DataProcessing.py RAW_rcp85_" + lat +"_" + lon +".csv rcp85_" + lat +"_" + lon +".csv")
-
-time.sleep(120)
+#
+# time.sleep(120)
