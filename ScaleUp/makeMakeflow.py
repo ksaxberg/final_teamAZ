606421e59486aff47bb0582d8e393cef4d2eb82b
Kyle <saxberg@email.arizona.edu>
2016-12-08 10:36:49 -0700

    Changing makeflow running directory assumption

--- ScaleUp/makeMakeflow.py
+++ ScaleUp/makeMakeflow.py
@@ -9,7 +9,6 @@ with open("Makeflow.makeflow", "w") as f:
     run = ""
     for line in files:
         if (line.endswith(".csv")): 
-            run = "{0}: {1} Get_MoLS.sh Run_Model.py\n".format(line.strip(), os.path.join(arg, line.strip()))
-            run += "    bash ./Get_MoLS.sh\n"
-            run += "    python Run_Model.py {0}\n\n".format(line.strip())
+            run = "{0}.out->weather.csv: {0}->weather.csv Get_MoLS.sh Run_Model.py\n".format(line.strip()) 
+            run += "    bash ./Get_MoLS.sh ; python Run_Model.py weather.csv\n\n".format(line.strip())
             f.write(run)
