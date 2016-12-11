294e43e28ff7a017de603b796dac5324d7102487
Adele Kapellusch <akoutia@email.arizona.edu>
2016-12-11 11:30:55 -0700

    Updated DataCollection

--- DataCollection/mergeCSVs.py
+++ DataCollection/mergeCSVs.py
@@ -6,7 +6,7 @@ import sys
 
 args = sys.argv[1:]
 scenario = args[0]
-
+errors = "erros.txt"
 if args[0] == '-h':
     print 'mergeCSVS.py takes one argument:'
     print 'SCENARIO (rcp45 or rcp85)'
@@ -15,7 +15,7 @@ if args[0] == '-h':
 
 
 def combine_data(scen):
-    with open('filteredArizonaCoords.csv','r') as coords:
+    with open('filteredFloridaCoords.csv','r') as coords:
         lines = coords.readlines()
     coordinates = []
     for line in lines:
@@ -28,24 +28,27 @@ def combine_data(scen):
 
     os.system('mkdir ' + scen)
     os.system('mv *' + scen + '* ' + scen + '/')
-    os.system('mkdir ' + scen +'Data')
-
-    for coor in coordinates:
-        lat = coor[0]
-        lon = str(float(coor[1]))
-        histInFile = 'historical/historical'+'_'+lat+'_'+lon+'.csv'
-        futInFile = scen +"/" + scen +'_'+lat+'_'+lon+'.csv'
-        outfile = scen + 'Data/' + scen + '_'+lat+'_'+lon+'.csv'
-        with open(outfile,'w') as output:
-            with open(histInFile,'r') as histIn:
-                histData = histIn.readlines()
-            for histd in histData:
-                output.write(histd)
-            with open(futInFile,'r') as futIn:
-                futData = futIn.readlines()
-            for futd in futData:
-                if futd != futData[0]:
-                    output.write(futd)
+    os.system('mkdir ' + scen +'DataFlorida')
+    with open(errors,'w') as e:
+        for coor in coordinates:
+            lat = coor[0]
+            lon = str(float(coor[1]))
+            histInFile = 'historical/historical'+'_'+lat+'_'+lon+'.csv'
+            futInFile = scen +"/" + scen +'_'+lat+'_'+lon+'.csv'
+            outfile = scen + 'DataFlorida/' + scen + '_'+lat+'_'+lon+'.csv'
+            try:
+                with open(outfile,'w') as output:
+                    with open(histInFile,'r') as histIn:
+                        histData = histIn.readlines()
+                    for histd in histData:
+                        output.write(histd)
+                    with open(futInFile,'r') as futIn:
+                        futData = futIn.readlines()
+                    for futd in futData:
+                        if futd != futData[0]:
+                            output.write(futd)
+            except Exception as exp:
+                e.write(str(exp)+"\n")
 
 answer = raw_input('Are you in the directory with all of your CSVs (historical, rcp45, and rcp85)? y or n?')
 if answer == 'y' or answer == 'Y':
