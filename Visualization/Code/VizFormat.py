5a8f308ba318b11a63b8175273571c94405cc0d5
Adele Kapellusch <akoutia@email.arizona.edu>
2016-12-12 14:17:06 -0700

    Updated Visualization

--- Visualization/Code/VizFormat.py
+++ Visualization/Code/VizFormat.py
@@ -9,15 +9,14 @@ outputFile = args[1]  #Path to where you want the output CSV to be
 files = os.listdir(pathToData)
 
 with open(outputFile,'w') as outF:
-    #(0)year(1)month(2)day(3)maximum temperature(4)minimum temperature(5)precipitation in mm(6)average temperature
-    # (7)precipitation in cm (8)relative humidity(9)Abundance
+
     outF.write('Lat,Lon,Time,T_max,T_min,Prcp(mm),T_ave,Prcp(cm),Rh_ave,Abundance\n')
 
     for f in files:
         if f != '.DS_Store':
             print f
             fName = f.split('_')
-            #rcp45_31.35_-109.51.csv.out
+
             lat = fName[1]
             lonIndex = (fName[2]).index('.c')
             lon =fName[2][:lonIndex]
@@ -33,6 +32,7 @@ with open(outputFile,'w') as outF:
                     outF.write(lat+','+lon+','+row[0]+'-'+row[1]+'-'+row[2]+','+row[3]+','+row[4]+','+row[5]+','+row[6]+','+
                                row[7]+','+row[8]+','+row[9])
             except(Exception):
-                print f
+                with open('errors_Viz.txt','a') as errorlog:
+                    errorlog.write(f + '\n')
 
 
