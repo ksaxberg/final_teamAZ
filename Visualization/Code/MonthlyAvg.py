6f8d12f0d2635146704296f7dbc29c84d18674e5
Adele Kapellusch <akoutia@email.arizona.edu>
2016-12-11 20:33:33 -0700

    Updated Documentation

--- Visualization/Code/MonthlyAvg.py
+++ Visualization/Code/MonthlyAvg.py
@@ -1,13 +1,26 @@
+"""Takes the full set of data and takes monthly averages of desired variable (default = abundance)"""
+
+import sys
+
+args = sys.argv[1:]
+
+inputPath = sys[0]   #EXAMPLE: rcp45_Arizona.csv
+outputPath = sys[1]  #EXAMPLE: MonthlyTemp.csv
+if len(args) == 2:
+    variable = 'Abundance'
+elif len(args) == 3:
+    variable = args[2]
+
 def GetSingleColumnMonthlyAvg(columnName):
 
     columnIndex = None
     currentYearMonth = None
     pointsToAvg = []
 
-    with open("MonthlyTemp.csv", "w") as writer:
+    with open(outputPath, "w") as writer:
         writer.write("Lat,Lon,Time,"+columnName+"\n")
 
-        with open("/Users/danielkapellusch/Desktop/TESTrcp45Data.csv", "r") as reader:
+        with open(inputPath, "r") as reader:
             for line in enumerate(reader.readlines()):
                 data = line[1].split(",")
                 if (not columnIndex) or data[0] == "Lat":
@@ -27,4 +40,4 @@ def GetSingleColumnMonthlyAvg(columnName):
                                                      sum(map(float, pointsToAvg)) / len(pointsToAvg)))
 
 
-GetSingleColumnMonthlyAvg("T_ave")
+GetSingleColumnMonthlyAvg(variable)
