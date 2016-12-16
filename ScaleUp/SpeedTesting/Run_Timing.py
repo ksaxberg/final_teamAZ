aa34ba810e2efc2a9d52ab4902d1e005ccc0d0a7
Kyle <saxberg@email.arizona.edu>
2016-12-16 11:58:47 -0700

    Cleaned some files, added speedtesting. Still need to test the speedtesting though

---
+++ ScaleUp/SpeedTesting/Run_Timing.py
@@ -0,0 +1,25 @@
+from timeit import defualt_timer as timer
+from MoLS import Run_Model
+import os
+import shutil
+
+singleYear = "./singleYear.csv"
+fiveYear = "./fiveYear.csv"
+yearTimings = []
+for i in range(5):
+    newFileName = singleYear[-4]+str(i)+".csv")
+    shutil.copyfile(singleYear, newFileName)
+    start = timer()
+    Run_Model.model(os.getcwd(), newFileName[-4])
+    end = timer()
+    yearTimings.append(end-start)
+newFiveYearName = fiveYear[-4]+"1"+".csv"
+shutil.copyfile(fiveYear, newFiveYearName) 
+start = timer()
+Run_Model.model(os.getcwd(), newFiveYearName[-4])
+end = timer()
+fiveYearTime = end-start
+with open("timings.csv", "w") as f:
+    for i in range(len(yearTimings)):
+        f.write(yearTimings[i])
+    f.write(fiveYearTime+"\n")
