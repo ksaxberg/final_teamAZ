2e0ea8fabd893b0fe963af8a4723ed5a355ac37b
Chameleon Cloud User <cc@teamarizonatrenta-master.novalocal>
2016-12-16 19:33:19 +0000

    More robust speed testing

--- ScaleUp/SpeedTesting/Run_Timing.py
+++ ScaleUp/SpeedTesting/Run_Timing.py
@@ -1,25 +1,33 @@
 from timeit import default_timer as timer
+from datetime import datetime as dt
 from MoLS import Run_Model
 import os
 import shutil
 
-singleYear = "./singleYear.csv"
-fiveYear = "./fiveYear.csv"
-yearTimings = []
-for i in range(5):
-    newFileName = singleYear[:-4]+str(i)+".csv"
-    shutil.copyfile(singleYear, newFileName)
+
+if __name__=="__main__":
+    tasksStarted = dt.now()
+    singleYear = "./singleYear.csv"
+    fiveYear = "./fiveYear.csv"
+    yearTimings = []
+    for i in range(5):
+        newFileName = singleYear[:-4]+str(i)+".csv"
+        shutil.copyfile(singleYear, newFileName)
+        start = timer()
+        Run_Model.model(os.getcwd(), newFileName[:-4])
+        end = timer()
+        yearTimings.append(end-start)
+    newFiveYearName = fiveYear[:-4]+"1"+".csv"
+    shutil.copyfile(fiveYear, newFiveYearName) 
     start = timer()
-    Run_Model.model(os.getcwd(), newFileName[:-4])
+    Run_Model.model(os.getcwd(), newFiveYearName[:-4])
     end = timer()
-    yearTimings.append(end-start)
-newFiveYearName = fiveYear[:-4]+"1"+".csv"
-shutil.copyfile(fiveYear, newFiveYearName) 
-start = timer()
-Run_Model.model(os.getcwd(), newFiveYearName[:-4])
-end = timer()
-fiveYearTime = end-start
-with open("timings.csv", "w") as f:
-    for i in range(len(yearTimings)):
-        f.write(str(yearTimings[i])+"\n")
-    f.write(str(fiveYearTime)+"\n")
+    fiveYearTime = end-start
+    tasksFinished = dt.now()
+    with open("timings.csv", "w") as f:
+        f.write(tasksStarted.strftime("%Y-%m-%d %H:%M:%S.%f"))
+        f.write("\n")
+        for i in range(len(yearTimings)):
+            f.write(str(yearTimings[i])+"\n")
+        f.write(str(fiveYearTime)+"\n")
+        f.write(tasksFinished.strftime("%Y-%m-%d %H:%M:%S.%f"))
